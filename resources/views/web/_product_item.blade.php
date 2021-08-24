c09ab4f83644dfc543eb984fe7397717645223b6
Andres Felipe Fonseca Mosquera <andrespipefon@hotmail.com>
2021-08-23 20:21:23 -0500

    Vista de productos para los clientes

--- resources/views/web/_product_item.blade.php
+++ resources/views/web/_product_item.blade.php
@@ -3,14 +3,14 @@
     <div class="product-item fix mb-30">
         <div class="product-thumb">
             <a href="product-details.html">
-                <img src="{{$product->images->pluck('url')[0]}}" class="img-pri" alt="">
-                <img src="{{$product->images->pluck('url')[1]}}" class="img-sec" alt="">
+                <img src="{{$product->images->pluck('url')[0]}}" class="img-pri" alt="{{$product->name}}">
+                <img src="{{$product->images->pluck('url')[1]}}" class="img-sec" alt="{{$product->name}}">
             </a>
-            <div class="product-label">
+            <!-- <div class="product-label">
                 <span>hot</span>
-            </div>
+            </div> -->
             <div class="product-action-link">
-                <a href="#" data-toggle="modal" data-target="#quick_view"> <span data-toggle="tooltip"
+                <a href="#" data-toggle="modal" data-target="#quick_view{{$product->id}}"> <span data-toggle="tooltip"
                         data-placement="left" title="Quick view"><i class="fa fa-search"></i></span> </a>
                 <a href="#" data-toggle="tooltip" data-placement="left" title="Wishlist"><i
                         class="fa fa-heart-o"></i></a>
@@ -21,9 +21,9 @@
             </div>
         </div>
         <div class="product-content">
-            <h4><a href="product-details.html">vertual product 01</a></h4>
+            <h4><a href="product-details.html">{{$product->name}}</a></h4>
             <div class="pricebox">
-                <span class="regular-price">$70.00</span>
+                <span class="regular-price">${{$product->sell_price}}</span>
                 <div class="ratings">
                     <span class="good"><i class="fa fa-star"></i></span>
                     <span class="good"><i class="fa fa-star"></i></span>
@@ -42,15 +42,15 @@
     <div class="product-list-item mb-30">
         <div class="product-thumb">
             <a href="product-details.html">
-                <img src="{{$product->images->pluck('url')[0]}}" class="img-pri" alt="">
-                <img src="{{$product->images->pluck('url')[1]}}" class="img-sec" alt="">
+                <img src="{{$product->images->pluck('url')[0]}}" class="img-pri" alt="{{$product->name}}">
+                <img src="{{$product->images->pluck('url')[1]}}" class="img-sec" alt="{{$product->name}}">
             </a>
-            <div class="product-label">
+            <!-- <div class="product-label">
                 <span>hot</span>
-            </div>
+            </div> -->
         </div>
         <div class="product-list-content">
-            <h3><a href="product-details.html">virtual product 02</a></h3>
+            <h3><a href="product-details.html">{{$product->name}}</a></h3>
             <div class="ratings">
                 <span class="good"><i class="fa fa-star"></i></span>
                 <span class="good"><i class="fa fa-star"></i></span>
@@ -62,18 +62,14 @@
                 </div>
             </div>
             <div class="pricebox">
-                <span class="regular-price">$70.00</span>
-                <span class="old-price"><del>$90.00</del></span>
+                <span class="regular-price">${{$product->sell_price}}</span>
+                <!-- <span class="old-price"><del>$90.00</del></span> -->
             </div>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-            <p>Phasellus id nisi quis justo tempus mollis sed et dui. In hac habitasse platea
-                dictumst. Suspendisse ultrices mauris diam. Nullam sed aliquet elit. Mauris
-                consequat nisi ut mauris efficitur lacinia.</p>
+            <p>{{$product->short_description}}</p>
             <div class="product-list-action-link">
                 <a class="buy-btn" href="#" data-toggle="tooltip" data-placement="top" title="Add to cart">go to buy <i
                         class="fa fa-shopping-cart"></i> </a>
-                <a href="#" data-toggle="modal" data-target="#quick_view"> <span data-toggle="tooltip"
+                <a href="#" data-toggle="modal" data-target="#quick_view{{$product->id}}"> <span data-toggle="tooltip"
                         data-placement="top" title="Quick view"><i class="fa fa-search"></i></span> </a>
                 <a href="#" data-toggle="tooltip" data-placement="top" title="Wishlist"><i
                         class="fa fa-heart-o"></i></a>
@@ -83,3 +79,69 @@
     </div>
     <!-- product single list item start -->
 </div> <!-- product single column end -->
+<!-- Quick view modal start -->
+<div class="modal" id="quick_view{{$product->id}}">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <!-- product details inner end -->
+                <div class="product-details-inner">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <div class="product-large-slider slick-arrow-style_2 mb-20">
+                                @foreach($product->images as $imagen)
+                                <div class="pro-large-img">
+                                    <img src="{{$imagen->url}}" alt="" />
+                                </div>
+                                @endforeach
+                            </div>
+                            <div class="pro-nav slick-padding2 slick-arrow-style_2">
+                                @foreach($product->images as $image)
+                                <div class="pro-nav-thumb"><img src="{{$image->url}}" alt="" />
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="product-details-des mt-md-34 mt-sm-34">
+                                <h3><a href="product-details.html">{{$product->name}}</a></h3>
+                                <div class="ratings">
+                                    <span class="good"><i class="fa fa-star"></i></span>
+                                    <span class="good"><i class="fa fa-star"></i></span>
+                                    <span class="good"><i class="fa fa-star"></i></span>
+                                    <span class="good"><i class="fa fa-star"></i></span>
+                                    <span><i class="fa fa-star"></i></span>
+                                    <div class="pro-review">
+                                        <span>1 review(s)</span>
+                                    </div>
+                                </div>
+                                <div class="availability mt-10">
+                                    <h5>Availability:</h5>
+                                    <span>{{$product->stock}} en stock</span>
+                                </div>
+                                <div class="pricebox">
+                                    <span class="regular-price">${{$product->sell_price}}</span>
+                                </div>
+                                <p>{{$product->short_description}}</p>
+                                <div class="quantity-cart-box d-flex align-items-center mt-20">
+                                    <div class="quantity">
+                                        <div class="pro-qty"><input type="text" value="1"></div>
+                                    </div>
+                                    <div class="action_link">
+                                        <a class="buy-btn" href="#">add to cart<i class="fa fa-shopping-cart"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- product details inner end -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Quick view modal end -->
